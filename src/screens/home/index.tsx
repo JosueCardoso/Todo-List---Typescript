f3aad3c227abad74d8924b6c21648522d5bee68d
Josue Cardoso <josuegcardoso@outlook.com>
2021-01-16 16:43:03 -0300

    Criado estrutura basica e alterado regra de TS

---
+++ src/screens/home/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+import { Container } from './styles';
+
+const home: React.FC = () => {
+    return (
+        <Container>
+            <Text>Teste</Text>
+        </Container>
+    );
+}
+
+export default home;
